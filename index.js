dd2e6e6bb561db57846127724c046b7dd092e50e
MisHersheys <aquividopau@gmail.com>
2024-12-07 15:01:35 +0800

    added some js

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const hamburger = document.querySelector('.hamburger');
     hamburger.addEventListener('click', () => {
       navLinks.classList.toggle('active');
     });
+
+const container = document.querySelector('.container');
+const loginhere = document.querySelector('.loginhere');
+const signuphere = document.querySelector('.signuphere');
+
+signuphere.addEventListener('click', ()=> {
+  container.classList.add('active');
+});
+
+loginhere.addEventListener('click', ()=> {
+  container.classList.remove('acive');
+});
