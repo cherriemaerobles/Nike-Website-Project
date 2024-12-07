ecf71e5085fc29ae88628daf04b220799b3b890b
MisHersheys <aquividopau@gmail.com>
2024-12-07 19:00:13 +0800

    fix some errors

--- script.js
+++ script.js
@@ -5,19 +5,36 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+const modalOverlay = document.querySelector('.modal-overlay');
 const container = document.querySelector('.container');
 const registerlink = document.querySelector('.register-link');
 const loginlink = document.querySelector('.login-link');
-const btnlogin = document.querySelector('.btnlogin');
+const btn = document.querySelector('.fa-solid fa-user');
+
 
 registerlink.addEventListener('click', () => {
-  container.classList.add('active');
+  modalOverlay.style.display = 'flex'; 
+  container.classList.add('active'); 
 });
+
 
 loginlink.addEventListener('click', () => {
-  container.classList.remove('acive');
+  container.classList.remove('active');
+  setTimeout(() => {
+    modalOverlay.style.display = 'none'; 
+  }, 300); 
 });
 
-btnlogin.addEventListener('click', () => {
+
+modalOverlay.addEventListener('click', (e) => {
+  if (e.target === modalOverlay) {
+    container.classList.remove('active');
+    setTimeout(() => {
+      modalOverlay.style.display = 'none';
+    }, 300);
+  }
+});
+
+btn.addEventListener('click', () => {
   container.classList.add('active-popup');
 });
