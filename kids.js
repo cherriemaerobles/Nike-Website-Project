7413dafb4c14c6e61cef330951650368917100c0
lynsaguan <indiciolielyn@gmail.com>
2024-12-08 12:11:57 +0800

    deleted showcart class

--- kids.js
+++ kids.js
@@ -1,23 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the necessary elements
+
     const cartIcon = document.querySelector(".fa-cart-shopping");
     const cartTab = document.querySelector(".cartTab");
     const closeBtn = cartTab.querySelector(".close");
 
-    // Toggle the cart visibility when the cart icon is clicked
+
     cartIcon.addEventListener("click", function() {
-        // Check if the cart is currently open or closed
-        if (cartTab.style.transform === "translateX(0)") {
-            // Close the cart
+
+        if (cartTab.style.transform === "translateX(0)") {   
             cartTab.style.transform = "translateX(100%)";
-        } else {
-            // Open the cart
+        } else {    
             cartTab.style.transform = "translateX(0)";
         }
     });
 
-    // Close the cart when the close button is clicked
     closeBtn.addEventListener("click", function() {
         cartTab.style.transform = "translateX(100%)";
     });
 });
+
+
