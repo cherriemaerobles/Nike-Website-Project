e553ceaa6689dd1e346c7297267f282bf7d00a09
lynsaguan <indiciolielyn@gmail.com>
2024-12-08 13:11:10 +0800

    deleted spaces in js

--- kids.js
+++ kids.js
@@ -1,41 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
 
     const cartIcon = document.querySelector(".fa-cart-shopping");
     const cartTab = document.querySelector(".cartTab");
     const closeBtn = cartTab.querySelector(".close");
 
-    cartIcon.addEventListener("click", function () {
 
-        if (cartTab.style.transform === "translateX(0)") {
+    cartIcon.addEventListener("click", function() {
+
+        if (cartTab.style.transform === "translateX(0)") {   
             cartTab.style.transform = "translateX(100%)";
-        } else {
+        } else {    
             cartTab.style.transform = "translateX(0)";
         }
     });
 
-    closeBtn.addEventListener("click", function () {
+    closeBtn.addEventListener("click", function() {
         cartTab.style.transform = "translateX(100%)";
     });
 });
-
-
 function addToCart(productName, productPrice, productImage) {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const product = { name: productName, price: productPrice, image: productImage, quantity: 1 };
-    cart.push(product);
-    localStorage.setItem('cart', JSON.stringify(cart));
+    const existingProduct = cart.find(item => item.name === productName);
+    
+    if (existingProduct) {
+        existingProduct.quantity++;
+    } else {
+        const product = { name: productName, price: productPrice, image: productImage, quantity: 1 };
+        cart.push(product);
+    }
 
+    localStorage.setItem('cart', JSON.stringify(cart));
     updateCartCount();
     loadCartItems();
 }
-
 
 function updateCartCount() {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
     const cartCount = document.getElementById('cart-count');
     cartCount.textContent = cart.length;
 }
-
 
 function loadCartItems() {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
@@ -51,14 +54,12 @@ function loadCartItems() {
             <div class="name">${item.name}</div>
             <div class="totalPrice">₱${item.price * item.quantity}</div>
             <div class="quantity">
-                <span class="minus" onclick="updateQuantity('${item.name}', -1)"><</span>
+                <span class="minus" onclick="updateQuantity('${item.name}', -1)">-</span>
                 <span>${item.quantity}</span>
-                <span class="plus" onclick="updateQuantity('${item.name}', 1)">></span>
+                <span class="plus" onclick="updateQuantity('${item.name}', 1)">+</span>
             </div>
-        `;
+        `;    
         cartItemsContainer.appendChild(itemElement);
-
-
         totalPrice += item.price * item.quantity;
     });
 
