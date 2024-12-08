911c03cd4175d2962192d383832220d771b7d06e
MisHersheys <aquividopau@gmail.com>
2024-12-08 21:25:08 +0800

    changes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const registerlink = document.querySelector('.register-link');
 const loginlink = document.querySelector('.login-link');
-const btn = document.querySelector('.fa-solid fa-user');
 
 
 registerlink.addEventListener('click', () => {
@@ -11,11 +10,6 @@ registerlink.addEventListener('click', () => {
 
 loginlink.addEventListener('click', () => {
   wrapper.classList.remove('active');
-});
-
-
-btn.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
 });
 
 document.getElementById('openModal').addEventListener('click', function () {
