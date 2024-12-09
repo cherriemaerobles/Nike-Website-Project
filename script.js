be577aeaf20ab68e150ceb8593a005845370b5ca
MisHersheys <aquividopau@gmail.com>
2024-12-09 14:17:41 +0800

    some changes in script

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+
+const openModalButton = document.getElementById('.openModal');
+const closeModalButton = document.getElementById('.closeModal');
+const modal = document.getElementById('.modal');
+
+
 const wrapper = document.querySelector('.wrapper');
-const registerlink = document.querySelector('.register-link');
-const loginlink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
 
 
-registerlink.addEventListener('click', () => {
-  wrapper.classList.add('active'); 
+openModalButton.addEventListener('click', () => {
+  modal.classList.add('active');
 });
 
 
-loginlink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
+closeModalButton.addEventListener('click', () => {
+  modal.classList.remove('active');
 });
 
-document.getElementById('openModal').addEventListener('click', function () {
-  document.getElementById('modal').classList.add('active');
-});
 
-document.getElementById('closeModal').addEventListener('click', function () {
-  document.getElementById('modal').classList.remove('active');
-});
-
-window.addEventListener('click', function (event) {
-  const modal = document.getElementById('modal');
+window.addEventListener('click', (event) => {
   if (event.target === modal) {
-      modal.classList.remove('active');
+    modal.classList.remove('active');
   }
+});
+
+registerLink.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  wrapper.classList.remove('active');
 });
